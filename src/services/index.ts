ab50c8b3193d6318392a97c08335a4717132ca3e
Roy Abanilla <royabanilla@gmail.com>
2023-01-24 00:33:50 +0800

    [RA 01/24/23] - delete unnecessary codes

--- src/services/index.ts
+++ src/services/index.ts
@@ -16,26 +16,6 @@ class CatDataService {
   getCatDetailById(id: string): Promise<CatDetailModel> {
     return http.get(`https://api.thecatapi.com/v1/images/${id}`);
   }
-
-  //   create(data: any): Promise<any> {
-  //     return http.post("/tutorials", data);
-  //   }
-
-  //   update(id: any, data: any): Promise<any> {
-  //     return http.put(`/tutorials/${id}`, data);
-  //   }
-
-  //   delete(id: any): Promise<any> {
-  //     return http.delete(`/tutorials/${id}`);
-  //   }
-
-  //   deleteAll(): Promise<any> {
-  //     return http.delete(`/tutorials`);
-  //   }
-
-  //   findByTitle(title: string): Promise<any> {
-  //     return http.get(`/tutorials?title=${title}`);
-  //   }
 }
 
 export default new CatDataService();
